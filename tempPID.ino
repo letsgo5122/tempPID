48a0eebde7dd5e77febc8f706b2d1f64ac55ccb8
letsgo5122 <letsgo5122@gmail.com>
2020-10-20 15:20:49 +0800

    menu

--- tempPID.ino
+++ tempPID.ino
@@ -4,11 +4,7 @@ LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
 #define seconds(x) (x * 1000) //x seconds
 #define RELAY 13
-#define BUTTON A1
-//#define ECROTA 2
-//#define ECROTB 3
-#define ecClk 2
-#define ecData 3
+
 
 //Temperature sensor
 #define Hotend(x) uint8_t(x)  //x = Anolog 0 ~ 5
@@ -18,12 +14,17 @@ float logR2, R2, T, Tc, Tf;
 float c1 = 1.009249522e-03, c2 = 2.378405444e-04, c3 = 2.019202697e-07;
 
 //LCD 
+#define ecClk 2
+#define ecData 3
+#define ecBT A1
 double lcdDisplayCounter = 0;// ms
 bool RefreshLCD = 0;
 int m_w;
-int ecState;
-int ecLastState;
-int ecCounter;
+int ecRotState;
+int ecRotLastState;
+float ecCounter;
+bool ecBtPushed = 0;
+String menu="main";
 
 //PID
 double Setpoint, Output ,currentTemp;
@@ -33,31 +34,31 @@ unsigned long windowStartTime;
 int sampleTime = 100;
 unsigned long lastTime;
 double pErr, iErr, dErr, lastErr;
-int ecVal;
+float ecVal;
 float sec = 1000;
 float targetPoint, nextTargetTime, tempStep=0 ,elapse=0.0;
 
 
 void setup() {
   Serial.begin(9600);
+  lcd.begin(20, 4);
   pinMode(Hotend(0), INPUT);
   pinMode(RELAY, OUTPUT);
-  pinMode(BUTTON, INPUT_PULLUP);
- // pinMode(ECROTA, INPUT_PULLUP);
- // pinMode(ECROTB, INPUT_PULLUP);
+  pinMode(ecBT, INPUT_PULLUP);
   pinMode(ecClk, INPUT_PULLUP);
   pinMode(ecData, INPUT_PULLUP);
-  //attachInterrupt(digitalPinToInterrupt(ECROTA), rotA, FALLING);
-  //attachInterrupt(digitalPinToInterrupt(ECROTB), rotB, FALLING);
+
   //setPid(20,0.05,5);//45
   setPid(50,1.125,160);//45
   Setpoint = 25;
   ecVal = 45;
+  
   }
 
 void loop(){
-  encoderState();
-  isBtPush();//Reset some values
+  menuSelect(menu);
+  
+ // isBtPush();//Reset some values
   double Input = getTemp(Hotend(0));//A0
   currentTemp = Input;
 
@@ -70,18 +71,69 @@ void loop(){
   lcdDisplay();
   
   }
-  
-void encoderState(){
-  ecState = digitalRead(ecClk);
-  if(ecState != ecLastState){
-    if(digitalRead(ecData)!= ecState){
-      ecCounter ++;
+
+void menuSelect(String m){
+  ecRotState = digitalRead(ecClk);
+  if ( ecRotState!=ecRotLastState ){
+    if( digitalRead(ecData)!= ecRotState  ){
+      ecCounter = 0.5;// Run two times when rotary turned a tick      
     }
-    else ecCounter --;
+    else { 
+      ecCounter = -0.5;
+    }
+  
+    if ( m == "main"){ecVal += ecCounter;}
+    else if ( m == "Kp" ){Kp += ecCounter;}
+    else if ( m == "Ki" ){Ki += ecCounter * 0.01;}
+    else if( m == "Kd" ){Kd += ecCounter;}
+    else if( m == "start" ){ecVal += ecCounter;}
+    else ;
+    
+    ecRotLastState = ecRotState;
+    //ecVal += ecCounter;
+    if (ecCounter ==1 || ecCounter ==-1 ){ecCounter=0;}
   }
-  ecVal = ecCounter/2;
-  ecLastState = ecState;
-  }
+if (digitalRead(ecBT)==0 && m=="main"){
+    menu = "pid";
+    delay(500);//ecBT debounce
+    return;
+ }
+ if (digitalRead(ecBT)==0 && m=="pid"){
+    menu = "Kp";
+    delay(500);//ecBT debounce
+    return;
+ }
+  if (digitalRead(ecBT)==0 && m=="Kp"){
+    menu = "Ki";
+    delay(500);//ecBT debounce
+    return;
+ }
+   if (digitalRead(ecBT)==0 && m=="Ki"){
+    menu = "Kd";
+    delay(500);//ecBT debounce
+    return;
+ }
+ 
+ if (digitalRead(ecBT)==0 && m=="Kd"){//ecBT pushed
+    menu = "start";
+    delay(500);//ecBT debounce
+    return;
+ }
+ if (digitalRead(ecBT)==0 && m=="start"){
+    menu = "main";
+    delay(500);//ecBT debounce    elapse = 0;
+    WindowSize = 1000; sampleTime = 100;
+    tempStep = 3;
+    pErr=0; iErr=0; dErr=0;
+    //getTemp(Hotend(A0))
+    targetPoint = getTemp(Hotend(0)) + tempStep;// + 1 nextTemperature 
+    Setpoint = ecVal;
+    return;
+ }
+ 
+}
+
+
 float nextTemperature(float currentTemp){
   if ( currentTemp + 10 >= Setpoint ){
    WindowSize=2000; sampleTime=1000, tempStep=1;
@@ -98,20 +150,6 @@ float nextTemperature(float currentTemp){
 }
 
 
-
-void isBtPush(){
-  if (digitalRead(BUTTON)==0){
-    elapse = 0;
-    WindowSize = 1000; sampleTime = 100;
-    tempStep = 3;
-    pErr=0; iErr=0; dErr=0;
-    //getTemp(Hotend(A0))
-    targetPoint = getTemp(Hotend(0)) + tempStep;// + 1 nextTemperature 
-    Setpoint = ecVal;
-    delay(5);
-  } 
-}
-
 void Compute(float targetPoint,float Input){
   unsigned long now = millis();
   double timeChange = now - lastTime;
@@ -122,9 +160,9 @@ void Compute(float targetPoint,float Input){
     Output = Kp * pErr + Ki * iErr + Kd * dErr;
     lastErr = pErr;
     lastTime = now;
-  }
-  
+  }  
 }
+
 void pidCtrl(){
    if (millis() - windowStartTime > WindowSize)
   { //time to shift the Relay Window
@@ -155,73 +193,99 @@ double getTemp(uint8_t pin){
   }
 
 
-
 void lcdDisplay(){
   double now = millis();
     if (now-lcdDisplayCounter >= seconds(1) || RefreshLCD){
-      lcd.begin(20, 4);
-      lcd.print(currentTemp);
-      lcd.print("C");
-      lcd.print(" Set:");
-      lcd.print(Setpoint,0);
-      lcd.print(" *:");
-      lcd.print(ecVal);
-      lcd.setCursor(0, 1);     
-      lcd.print("Output");
-      lcd.print(Output);
-      lcd.setCursor(0, 2);
-      lcd.print("iErr:");
-      lcd.print(iErr);
-      lcd.setCursor(0, 3);
-      lcd.print("Tg:");
-      lcd.print(targetPoint,0);
-      lcd.print(" elapse:");
-      lcd.print(elapse,0);
+      //lcd.begin(20, 4);
+      if(menu=="main"){
+        lcd.clear();
+        lcd.setCursor(0, 0);
+        lcd.print(currentTemp);
+        lcd.print("C");
+        lcd.print(" Set:");
+        lcd.print(int(Setpoint));
+        lcd.print(" *:");
+        lcd.print(int(ecVal));
+        lcd.setCursor(0, 1);     
+        lcd.print("Out:");
+        lcd.print(Output,0);
+        lcd.print(" Menu:");
+        lcd.print(menu);
+        lcd.setCursor(0, 2);
+        lcd.print("iEr:");
+        lcd.print(iErr);
+        lcd.setCursor(0, 3);
+        lcd.print("Tg:");
+        lcd.print(targetPoint,0);
+        lcd.print(" elapse:");
+        lcd.print(elapse,0);
       
+        //Serial.print("\t");
+        //Serial.print(" Output: ");
+        //Serial.println(Output);  
+      }
+      if(menu== "pid"){
+        lcd.clear();
+        lcd.setCursor(0, 0);
+        lcd.print(" Kp:");
+        lcd.print(Kp);
+        lcd.setCursor(0, 1);
+        lcd.print(" Ki:");
+        lcd.print(Ki);
+        lcd.setCursor(0, 2);
+        lcd.print(" Kd:");
+        lcd.print(Kd); 
+        lcd.setCursor(0, 3);
+        lcd.print("        Back");                          
+      }
+      if(menu== "Kp"){
+        lcd.clear();
+        lcd.setCursor(0, 1);
+        lcd.print("Kp:");
+        lcd.print(Kp);
+        lcd.setCursor(0, 3);
+        lcd.print("        Back");  
+      }
+      if(menu== "Ki"){
+        lcd.clear();
+        lcd.setCursor(0, 1);
+        lcd.print("Ki:");
+        lcd.print(Ki);
+        lcd.setCursor(0, 3);
+        lcd.print("        Back");  
+      }
+      if(menu== "Kd"){
+        lcd.clear();
+        lcd.setCursor(0, 1);
+        lcd.print("Kd:");
+        lcd.print(Kd);
+        lcd.setCursor(0, 3);
+        lcd.print("        Back");  
+      }
+      if(menu== "start"){
+        lcd.clear();
+        lcd.setCursor(0, 0);
+        lcd.print(currentTemp);
+        lcd.print("C");
+        lcd.print(" Set:");
+        lcd.print(int(Setpoint));
+        lcd.print(" *:");
+        lcd.print(int(ecVal));
+        lcd.setCursor(0, 1);     
+        lcd.print("Output");
+        lcd.print(Output,0);
+        lcd.print(" MI:");
+        lcd.print(menu);
+        lcd.print("        Back");                          
+      }
+
       Serial.print("Tc: ");
       Serial.println(currentTemp);
-      //Serial.print("\t");
-      //Serial.print(" Output: ");
-      //Serial.println(Output);
-
+      
       if (!RefreshLCD)elapse++;
-      RefreshLCD = 0;
-      lcdDisplayCounter = now;
+        RefreshLCD = 0;
+        lcdDisplayCounter = now;        
+
     }
+    
   }
-
-//void rotA(){
-//  uint8_t interval = 50;
-//  bool t1=0, t2=0, t3=0, t4=0;
-//  while (interval > 0) {
-//    if (!digitalRead(ECROTA) && digitalRead(ECROTB)) t1 = 1;
-//    delay(1);
-//    if (!digitalRead(ECROTA) && !digitalRead(ECROTB)) t2 = 1;
-//    delay(1);
-//    if (digitalRead(ECROTA) && !digitalRead(ECROTB)) t3 = 1;
-//    delay(1);
-//    if (digitalRead(ECROTA) && digitalRead(ECROTB)) t4 = 1;
-//    delay(1);
-//    if (t1 && t2 && t3 && t4){ecVal ++; break;}
-//    interval--;
-//    RefreshLCD=1;
-//  }
-//}
-
-//void rotB(){
-//  uint8_t interval = 50;
-//  bool t1=0, t2=0, t3=0, t4=0;
-//  while (interval > 0) {
-//    if (digitalRead(ECROTA) && !digitalRead(ECROTB)) t1 = 1;
-//    delay(5);
-//    if (!digitalRead(ECROTA) && !digitalRead(ECROTB)) t2 = 1;
-//    delay(5);
-//    if (!digitalRead(ECROTA) && digitalRead(ECROTB)) t3 = 1;
-//    delay(5);
-//    if (digitalRead(ECROTA) && digitalRead(ECROTB)) t4 = 1;
-//    delay(5);
-//    if (t1 && t2 && t3 && t4){ecVal --; break;}
-//    interval--;
-//    RefreshLCD=1;
-//  }
-//}
